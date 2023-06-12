74586024200d2876a443927ac08073c354acbca5
marlon1925 <marlon.lalangui@epn.edu.ec>
2023-06-12 15:07:38 -0500

    cambios server

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const app = express()
 app.set('port',process.env.port || 3000)
 app.set('views',path.join(__dirname, 'views'))
 
+
+app.engine('.hbs',engine({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname:'.hbs'
+}))
+app.set('view engine','.hbs')
+
 // Middlewares 
 app.use(express.urlencoded({extended:false}))
 
